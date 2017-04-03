34843900da6db72fa877c55689e1000312ea8572
Wang Han <416810799@qq.com>
2017-04-03 19:00:18 +0800

    service.sh: Pull brevent.sh as to the newest version

--- common/service.sh
+++ common/service.sh
@@ -2,20 +2,29 @@
 
 sleep 30
 
-brevent=me.piebridge.brevent
-lnld=`dumpsys package $brevent | grep legacyNativeLibraryDir`
+package=me.piebridge.brevent
+brevent=/data/local/tmp/brevent
+lnld=`dumpsys package $package | grep legacyNativeLibraryDir`
 if [ x"$lnld" == x"" ]; then
-    echo "please install $brevent" >&2
+    echo "please install $package" >&2
     exit 1
 else
     lib=`echo $lnld | dd bs=1 skip=23 2>/dev/null`
     path=`ls $lib/*/libbrevent.so`
     if [ ! -f "$path" ]; then
-        echo "please install latest $brevent" >&2
+        echo "please install latest $package" >&2
         exit 1
     else
-        echo "exec $path"
-        exec $path
+        rm -rf $brevent
+        cp $path $brevent
+        if echo $path | grep -q -v 64; then
+            rm -rf /data/local/tmp/app_process
+            if [ -x /system/bin/app_process32 ]; then
+                ln -s /system/bin/app_process32 /data/local/tmp/app_process
+                export PATH=/data/local/tmp:$PATH
+            fi
+        fi
+        exec $brevent
         exit 0
     fi
 fi
